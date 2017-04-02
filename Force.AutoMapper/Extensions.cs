1399e3dff7420c37864dd5532bbea052ebaaa5db
Andrey Pesoshin <pesoshin@hightech.today>
2017-04-03 00:26:22 +0300

    added support for instance-based mapper

--- Force.AutoMapper/Extensions.cs
+++ Force.AutoMapper/Extensions.cs
@@ -11,37 +11,40 @@ namespace Force.AutoMapper
     public static class Extensions
     {
         public static TProjection ProjectById<TKey, TEntity, TProjection>(
-            this IQueryable<TEntity> query,TKey id)
+            this IQueryable<TEntity> query, TKey id, IConfigurationProvider configurationProvider = null)
             where TKey : IComparable, IComparable<TKey>, IEquatable<TKey>
             where TProjection : class, IHasId<TKey>
-            where TEntity : class, IHasId<TKey> =>
-            query.ProjectTo<TProjection>().ById(id);
+            where TEntity : class, IHasId<TKey>
+            => query
+                .ProjectTo<TProjection>(configurationProvider)
+                .ById(id);
 
         public static IQueryable<TDest> ApplyProjectApplyAgain<TSource, TDest>(
-            this IQueryable<TSource> queryable, object spec)
+            this IQueryable<TSource> queryable, object spec, IConfigurationProvider configurationProvider = null)
             where TSource : class
             where TDest : class
             => queryable
                 .MaybeWhere(spec)
                 .MaybeOrderBy(spec)
-                .ProjectTo<TDest>()
+                .ProjectTo<TDest>(configurationProvider)
                 .MaybeWhere(spec)
                 .MaybeOrderBy(spec);
 
         public static IQueryable<TDest> ApplyProjectApplyAgainWithoutOrderBy<TSource, TDest>(
-            this IQueryable<TSource> queryable, object spec)
+            this IQueryable<TSource> queryable, object spec, IConfigurationProvider configurationProvider = null)
             where TSource : class
             where TDest : class
             => queryable
                 .MaybeWhere(spec)
-                .ProjectTo<TDest>()
+                .ProjectTo<TDest>(configurationProvider)
                 .MaybeWhere(spec);
 
-        public static PagedResponse<TDest> Paged<TEntity, TDest>(this IQueryable<TEntity> query, IPaging spec)
+        public static PagedResponse<TDest> Paged<TEntity, TDest>(this IQueryable<TEntity> query, IPaging spec,
+            IConfigurationProvider configurationProvider = null)
             where TEntity : class, IHasId
             where TDest : class, IHasId => query
                 .MaybeWhere(spec)
-                .ProjectTo<TDest>()
+                .ProjectTo<TDest>(configurationProvider)
                 .MaybeWhere(spec)
                 .MaybeOrderBy(spec)
                 .OrderByIdIfNotOrdered()
@@ -49,35 +52,43 @@ namespace Force.AutoMapper
 
         public static PagedResponse<TDest> Paged<TEntity, TDest>(this IQueryable<TEntity> query,
             IPaging paging, IQueryableOrder<TDest> queryableOrder, IQueryableFilter<TEntity> entitySpec = null,
-            IQueryableFilter<TDest> destSpec = null)
+            IQueryableFilter<TDest> destSpec = null, IConfigurationProvider configurationProvider = null)
             where TEntity : class, IHasId where TDest : class
             => query
                 .EitherOrSelf(entitySpec, x => x.Where(entitySpec))
-                .ProjectTo<TDest>()
+                .ProjectTo<TDest>(configurationProvider)
                 .EitherOrSelf(destSpec, x => x.Where(destSpec))
                 .OrderBy(queryableOrder)
                 .ToPagedResponse(paging);
 
-        public static TKey Create<TKey, TDto, TEntity>(this IUnitOfWork uow, TDto dto)
+        public static TKey Create<TKey, TDto, TEntity>(this IUnitOfWork uow, TDto dto, IMapper mapper = null)
             where TEntity : class, IHasId<TKey>
             where TKey : IComparable, IComparable<TKey>, IEquatable<TKey>
         {
-            var entity = Mapper.Map<TEntity>(dto);
+            var mapperInstance = mapper ?? Mapper.Instance;
+
+            var entity = mapperInstance.Map<TEntity>(dto);
             uow.Add(entity);
             uow.Commit();
+
             return entity.Id;
         }
 
         public static void Update<TKey, TEntity, TDto>(this IUnitOfWork uow, TKey id,
-            TDto dto)
+            TDto dto, IMapper mapper = null)
             where TEntity : class, IHasId<TKey>
             where TKey : IComparable, IComparable<TKey>, IEquatable<TKey>
         {
+            var mapperInstance = mapper ?? Mapper.Instance;
+
             var entity = uow.Find<TEntity>(id);
-            Mapper.Map(dto, entity);
+            mapperInstance.Map(dto, entity);
             uow.Commit();
         }
 
-        public static TDest Map<TDest>(this object obj) => Mapper.Map<TDest>(obj);
+        public static TDest Map<TDest>(this object obj, IMapper mapper = null) =>
+            mapper
+                .EitherOrSelf(x => x == null, _ => Mapper.Instance)
+                .Map<TDest>(obj);
     }
 }
