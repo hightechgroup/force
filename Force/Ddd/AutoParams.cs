04e8852e2f1fa6e9f8da029641eeb8b4471c3da7
max <max@hightech.today>
2018-03-02 14:03:04 +0300

    Working with AutoParams

---
+++ Force/Ddd/AutoParams.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Force.Ddd
+{
+    public class AutoParams<T>
+        : IQueryableFilter<T>
+        , IQueryableOrder<T>
+    
+        where T : class, IHasId
+    {
+        public IQueryable<T> Filter(IQueryable<T> query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IOrderedQueryable<T> Order(IQueryable<T> queryable)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
