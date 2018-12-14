8c2351766fff2530ed290d21cccc28823cdc5a64
Max Arshinov <max@hightech.group>
2018-12-14 19:32:39 +0300

    demo app

---
+++ Demo.WebApp/ClientApp/src/app/blog/blog.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'blog',
+  templateUrl: './blog.component.html'
+})
+export class BlogComponent {
+
+}
--- Demo.WebApp/Startup/Startup.cs
+++ Demo.WebApp/Startup/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -113,6 +114,19 @@ namespace Demo.WebApp.Startup
             
             app.UseHttpsRedirection();
             app.UseMvc();
+            
+            app.UseSpa(spa =>
+            {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
+            });
         }
     }
 }
