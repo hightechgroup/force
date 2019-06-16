3cc67dd7001a7f697ecadd0a953641a3f9d41235
max <max@hightech.today>
2019-06-16 23:33:22 +0300

    WIP

--- Demo.WebApp/Features/SoDemo.cs
+++ Demo.WebApp/Features/SoDemo.cs
@@ -2,7 +2,12 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Linq;
+using Castle.Core.Logging;
+using Demo.WebApp.Domain;
 using Force.Ddd;
+using Microsoft.AspNetCore.Http;
 
 namespace Demo.WebApp.Features
 {
@@ -25,7 +30,215 @@ namespace Demo.WebApp.Features
                     },
                     24 * 60 * 60, 24 * 60 * 60
                 );
+    }
 
+    // ReSharper disable UnassignedGetOnlyAutoProperty
+    // ReSharper disable UnusedMember.Global
+
+    public abstract class Document : EntityBase
+    {        
+        public UploadedFile File { get; }
+
+        public abstract void Update(object o);
+    }
+
+    public abstract class DriverLicense : Document
+    {        
+        public DriverLicenseNumber Number { get; }
+    }
+
+    public abstract class Cv : Document
+    {
+        public PhoneNumber PhoneNumber { get; }     
+        public string Summary { get; }
+    }
+    
+    public class UploadedFile
+    {
+    }
+
+    public class PhoneNumber
+    {
+    }
+
+    public class DriverLicenseNumber
+    {
+    }
+
+    public class OE
+    {
+        public void Update<T>(IHasFile<T> entity, IFormFile file, T data)
+        {
+            FileStorage fileStorage = null;
+            ILogger logger = null;
+            UploadedFile uploaded = fileStorage.Upload(file);
+            
+            try
+            {
+                entity.Update(uploaded, data);
+            }
+            catch (SqlException se)
+            {
+                try
+                {
+                    fileStorage.Delete(uploaded);
+                }
+                catch (FileStorageException fe)
+                {
+                    logger.Fatal(fe.Message);
+                    throw;
+                }
+                throw;
+            }
+        }
+        
+        public IEnumerable<Document> Fetch(IQueryable<Document> documents)
+        {
+            return documents
+                .Take(10)
+                .ToList(); 
+        }
+        public bool TryCancel(State<Order> orderState, out State<Order> newState)
+        {
+            IQueryable<Document> documents;
+
+            
+            switch (orderState)
+            {
+                case ICancellableOrder o:
+                    newState = o.Cancel();
+                    return true;
+                default:
+                    newState = null;
+                    return false;
+            }
+        }
+        
+        
+        public State<Order> Process(State<Order> orderState, User user)
+        {            
+            switch (orderState)
+            {
+                case UnvalidatedOrder o: return o.Validate();
+                case ValidatedOrder o:
+                    var priceCalculator = new PriceCalculator(user);
+                    var price = priceCalculator.Calculate(o);
+                    return o.SetPrice(price);
+                case PricedOrder o: 
+                    var deliveryManager = new DeliveryManager(user);
+                    var trackingUrl = deliveryManager.Ship(o);
+                    return o.Shipped(trackingUrl);
+                default: return orderState;
+            }
+        }
+    }
+
+    public class FileStorage
+    {
+        public UploadedFile Upload(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(UploadedFile uploaded)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class FileStorageException : Exception
+    {
+    }
+
+    public interface IHasFile<in T>
+    {
+        void Update(UploadedFile file, T data);
+    }
+
+    public interface ICancellableOrder
+    {
+        State<Order> Cancel();
+    }
+
+    public class DeliveryManager
+    {
+        public DeliveryManager(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public State<Order> Ship(PricedOrder pricedOrder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class PriceCalculator
+    {
+        public PriceCalculator(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal Calculate(ValidatedOrder validatedOrder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class UnvalidatedOrder: State<Order>
+    {
+        public ValidatedOrder Validate()
+        {
+            return new ValidatedOrder(Entity, DateTime.UtcNow);
+        }
+        
+        public UnvalidatedOrder(Order entity) : base(entity)
+        {
+            var a = SoDemo.GetStateName(null);
+        }
+    }
+    
+    public class ValidatedOrder: State<Order>
+    {
+        internal ValidatedOrder(Order entity, DateTime dateTime) : base(entity)
+        {
+        }
+
+        public State<Order> SetPrice(decimal price)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class PricedOrder: State<Order>
+    {
+        public PricedOrder(Order entity) : base(entity)
+        {
+        }
+
+        public State<Order> Shipped(State<Order> trackingUrl)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ShippedOrder: State<Order>
+    {
+        public ShippedOrder(Order entity) : base(entity)
+        {
+        }
+    }
+    
+    public class CancelledOrder: State<Order>
+    {
+        public ShippedOrder(Order entity) : base(entity)
+        {
+        }
+    }
+    
+    public class Order
+    {
     }
 
     public class UserName: ValueObject
