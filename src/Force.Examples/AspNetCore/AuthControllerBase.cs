cf2ec5ab4544e2913ec4352ca9aa45edd5b886d0
Angela <mustafina@hightech.group>
2021-02-17 14:27:35 +0300

    PAR-8955

---
+++ src/Force.Examples/AspNetCore/AuthControllerBase.cs
@@ -0,0 +1,15 @@
+﻿using Force.Examples.AspNetCore.Filters;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Force.Examples.AspNetCore
+{
+    [ApiController]
+    [ApiControllerFilter]
+    [Route("/api/[controller]/[action]")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class AuthControllerBase : ControllerBase
+    {
+    }
+}
